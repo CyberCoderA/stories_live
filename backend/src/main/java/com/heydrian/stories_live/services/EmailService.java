ff5d066035d064b3e6bf6aa91622fe06afbd9dbc
Adrian Pueblo <adrian.pueblo27@gmail.com>
2026-09-22 13:45:43 +0800

    Completed the email service.

--- backend/src/main/java/com/heydrian/stories_live/services/EmailService.java
+++ backend/src/main/java/com/heydrian/stories_live/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.heydrian.stories_live.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,18 @@ public class EmailService {
     @Autowired 
     private JavaMailSender mailSender;
 
+    @Value("${MAIL_USERNAME}")
+    private String fromUsername;
+
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         
+        message.setFrom(fromUsername); // Get the username from the .env file
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+        System.out.println("Email sent successfully!");
     }
 }
